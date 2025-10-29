6d1fde615d5212188699db18116a57cb4e3f010a
NACHO OFC <ignacioperezolavarria@gmail.com>
2025-10-28 21:09:50 -0300

    aaaaaaaaaa
    aaaaaaaaaaaaaaaaa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
-cd c:\Users\ignac\OneDrive\Escritorio\repositorios\DisproDEV
 
-git add pages/compass/index.vue nuxt.config.ts
-git commit -m "fix: Remove broken links and fix prerendering - enable SSR for static generation"
-git push origin main// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
   modules: ['@nuxtjs/tailwindcss'],
